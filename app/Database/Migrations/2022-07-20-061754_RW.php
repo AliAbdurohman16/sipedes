3e2ea3c659e61af7f83bd73cc5f98c16722adf46
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-08-11 13:21:08 +0700

    up

--- app/Database/Migrations/2022-07-20-061754_RW.php
+++ app/Database/Migrations/2022-07-20-061754_RW.php
@@ -8,11 +8,40 @@ class RW extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama_rw' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'no_rw' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'TIMESTAMP',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('rw');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('rw');
     }
 }
