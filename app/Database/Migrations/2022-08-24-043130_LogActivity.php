eb7f6682dafc988e63b0b92c442285d3f01b3d70
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-08-24 11:40:40 +0700

    Add Migrations LogActivity

---
+++ app/Database/Migrations/2022-08-24-043130_LogActivity.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class LogActivity extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+            ],
+            'information' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME'
+            ],
+            'updated_at' => [
+                'type'       => 'DATETIME'
+            ],
+            'deleted_at' => [
+                'type'       => 'DATETIME',
+                'null'       => true
+            ]
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('log_activity');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('log_activity');
+    }
+}
