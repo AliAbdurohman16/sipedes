2fae3e6b8d80c9d3b944fb9f18fa9c4147fb977f
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-09-04 23:48:12 +0700

    update meta in frontend

--- app/Views/template/frontend/index.php
+++ app/Views/template/frontend/index.php
@@ -3,9 +3,13 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="SIPEDES (Sistem Pelayanan Desa Cibinuang) merupakan aplikasi berbasis website untuk menyediakan pelayanan pengajuan tentang surat menyurat kepada masyarakat khususnya masyarakat Desa Cibinuang, Kec.Kuningan, Kab.Kuningan" />
+    <meta name="keywords" content="kabupaten kuningan, kuningan, kuninganmass, kab.kuningan, desa, desa kuningan, jawa barat, jabar juara" />
+    <meta name="author" content="PPK PBK FKOM UNIKU 2022" />
+    <meta name="email" content="pbk@uniku.ac.id" />
+    <meta name="website" content="https://uniku.ac.id" />
+    <meta name="Version" content="v1.0.0" />
     <title>SIPEDES - Desa Cibinuang</title>
     <!-- Favicon -->
     <link href="images/logo/logoSaja.png" rel="shortcut icon">
