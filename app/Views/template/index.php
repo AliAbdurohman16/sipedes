ddcd527db7b7a5c6a57a98d7eb61cbb6adc00d4f
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-09-04 23:41:43 +0700

    update typo

--- app/Views/template/index.php
+++ app/Views/template/index.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <title><?= $title ?> - Sistem Pelayanan Desa Cibinuang</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="SIPEDE (Sistem Pelayanan Desa Cibinuang) merupakan aplikasi berbasis website untuk menyediakan pelayanan pengajuan tentang surat menyurat kepada masyarakat khususnya masyarakat Desa Cibinuang, Kec.Kuningan, Kab.Kuningan" />
+    <meta name="description" content="SIPEDES (Sistem Pelayanan Desa Cibinuang) merupakan aplikasi berbasis website untuk menyediakan pelayanan pengajuan tentang surat menyurat kepada masyarakat khususnya masyarakat Desa Cibinuang, Kec.Kuningan, Kab.Kuningan" />
     <meta name="keywords" content="kabupaten kuningan, kuningan, kuninganmass, kab.kuningan, desa, desa kuningan, jawa barat, jabar juara" />
     <meta name="author" content="PPK PBK FKOM UNIKU 2022" />
     <meta name="email" content="pbk@uniku.ac.id" />
