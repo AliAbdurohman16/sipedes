5584c083259a4d84f07474844658f53090672b40
fikriks <fikriks130@gmail.com>
2022-09-04 17:49:23 +0700

    update: template login :rocket:

--- app/Views/template/auth/main.php
+++ app/Views/template/auth/main.php
@@ -1,19 +1,19 @@
 <!doctype html>
-<html lang="en" dir="ltr">
+<html lang="id" dir="ltr">
 
 <head>
     <meta charset="utf-8" />
-    <title><?= $title ?></title>
+    <title><?= $title ?> - Sistem Pelayanan Desa Cibinuang</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Premium Bootstrap 5 Landing Page Template" />
-    <meta name="keywords" content="Saas, Software, multi-uses, HTML, Clean, Modern" />
-    <meta name="author" content="Shreethemes" />
-    <meta name="email" content="support@shreethemes.in" />
-    <meta name="website" content="https://shreethemes.in" />
-    <meta name="Version" content="v4.2.0" />
+    <meta name="description" content="SIPEDE (Sistem Pelayanan Desa Cibinuang) merupakan aplikasi berbasis website untuk menyediakan pelayanan pengajuan tentang surat menyurat kepada masyarakat khususnya masyarakat Desa Cibinuang, Kec.Kuningan, Kab.Kuningan" />
+    <meta name="keywords" content="kabupaten kuningan, kuningan, kuninganmass, kab.kuningan, desa, desa kuningan, jawa barat, jabar juara" />
+    <meta name="author" content="PPK PBK FKOM UNIKU 2022" />
+    <meta name="email" content="pbk@uniku.ac.id" />
+    <meta name="website" content="https://uniku.ac.id" />
+    <meta name="Version" content="v1.0.0" />
 
     <!-- favicon -->
-    <link rel="shortcut icon" href="<?= base_url() ?>/images/logo/logoSaja.png" />
+    <link rel="shortcut icon" href="<?= base_url() ?>/images/logo/logo_sipedes.png" />
     <!-- Css -->
     <link href="<?= base_url() ?>/assets/libs/simplebar/simplebar.min.css" rel="stylesheet">
     <!-- Bootstrap Css -->
@@ -54,24 +54,27 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9.17.2/dist/sweetalert2.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
 
-
-    <script>
-        $(function() {
-            <?php if (session()->has("error_message")) { ?>
+    <?php if (session()->has("error_message")) { ?>
+        <script>
+            $(function() {
                 Swal.fire({
                     icon: 'error',
                     title: 'Oops...',
                     text: '<?= session("error_message") ?>'
                 })
-            <?php } else if (session()->has("success")) { ?>
+            });
+        </script>
+    <?php } else if (session()->has("success")) { ?>
+        <script>
+            $(function() {
                 Swal.fire({
                     icon: 'success',
                     title: 'Selamat!',
                     text: '<?= session("success") ?>'
                 })
-            <?php } ?>
-        });
-    </script>
+            });
+        </script>
+    <?php } ?>
 </body>
 
 </html>
