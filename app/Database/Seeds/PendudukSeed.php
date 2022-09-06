43d6b0d86a17d29cfe7d331a17a178210a70e38d
aliabdurohman16 <alyabdurohman16@gmail.com>
2022-09-06 12:40:09 +0700

    update pendudukSeed

--- app/Database/Seeds/PendudukSeed.php
+++ app/Database/Seeds/PendudukSeed.php
@@ -12,11 +12,11 @@ class PendudukSeed extends Seeder
         $faker = \Faker\Factory::create('id_ID');
         $pddkModel = new PendudukModel;
 
-        for($i = 1; $i < 20; $i++){
+        for($i = 1; $i < 50; $i++){
             $data = [
                 'nik' => rand(1000000000000000, 9999999999999999),
                 'name' => $faker->name(),
-                'jenis_kelamin' => 'L',
+                'jenis_kelamin' => $faker->randomElement(['L', 'P']),
                 'tempat_lahir' => $faker->city(),
                 'tgl_lahir' => $faker->date(),
                 'rt_id' => $i,
@@ -26,38 +26,11 @@ class PendudukSeed extends Seeder
                 'kabupaten' => 'Kuningan',
                 'provinsi' => 'Jawa Barat',
                 'alamat' => $faker->name(),
-                'gol_darah' => 'O',
+                'gol_darah' => $faker->randomElement(['A', 'AB', 'B', 'O']),
                 'agama' => 'Islam',
-                'status_kawin' => 'Belum',
-                'pendidikan_terakhir' => 'SMA',
-                'pekerjaan' => 'PNS',
-                'nama_ibu' => $faker->name('female'),
-                'nama_ayah' => $faker->name('male'),
-                'status' => 'ada',
-            ];
-
-            $pddkModel->insert($data);
-        }
-
-        for($i = 1; $i < 15; $i++){
-            $data = [
-                'nik' => rand(1000000000000000, 9999999999999999),
-                'name' => $faker->name(),
-                'jenis_kelamin' => 'P',
-                'tempat_lahir' => $faker->city(),
-                'tgl_lahir' => $faker->date(),
-                'rt_id' => $i,
-                'rw_id' => $i,
-                'kelurahan' => 'Cibinuang',
-                'kecamatan' => 'Kuningan',
-                'kabupaten' => 'Kuningan',
-                'provinsi' => 'Jawa Barat',
-                'alamat' => $faker->name(),
-                'gol_darah' => 'O',
-                'agama' => 'Islam',
-                'status_kawin' => 'Belum',
-                'pendidikan_terakhir' => 'SMA',
-                'pekerjaan' => 'PNS',
+                'status_kawin' => $faker->randomElement(['Belum', 'Sudah', 'Cerai Hidup', 'Cerai Mati']),
+                'pendidikan_terakhir' => $faker->randomElement(['SD', 'SMP', 'SMA', 'D3', 'S1', 'S2']),
+                'pekerjaan' => $faker->randomElement(['Petani', 'Nelayan', 'Guru', 'Pedagang', 'PNS']),
                 'nama_ibu' => $faker->name('female'),
                 'nama_ayah' => $faker->name('male'),
                 'status' => 'ada',
